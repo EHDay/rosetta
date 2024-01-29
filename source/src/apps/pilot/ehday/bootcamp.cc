775392648d2cf136e40f36113989d6b040cca3d0
Elijah Day <elijahday2021@gmail.com>
2024-01-29 16:25:42 -0600

    Added functionality to read in PDB and initialize an owning pointer to a pose.

--- source/src/apps/pilot/ehday/bootcamp.cc
+++ source/src/apps/pilot/ehday/bootcamp.cc
@@ -8,8 +8,25 @@
 // (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
 
 #include <iostream> 
+#include <basic/options/option.hh>
+#include <basic/options/keys/in.OptionKeys.gen.hh>
+#include <devel/init.hh>
+#include <core/import_pose/import_pose.hh>
+#include <utility/pointer/owning_ptr.hh>
+#include <core/pose/Pose.hh>
 
-int main() {
+int main( int argc, char ** argv) {
+	devel::init( argc, argv ) ;
+	utility::vector1< std::string > filenames = basic::options::option[
+	basic::options::OptionKeys::in::file::s ].value();
+	if ( filenames.size() > 0 ) {
+		std::cout << "You entered: " << filenames[ 1 ] << " as the PDB file to be read" << std::endl;
+	}
+	else {
+		std::cout << "You didn't provide a PDB file with the -in::file::s option" << std::endl;
+		return 1;
+	}
+	core::pose::PoseOP mypose = core::import_pose::pose_from_file( filenames[1] );
 	std::cout << "Hello World!" << std::endl;
 	return 0;
 }
